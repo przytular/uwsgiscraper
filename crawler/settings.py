dde5b23c40e39597848eb51b78dee93649ad119c
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-01-17 23:34:28 +0100

    nice settings

--- crawler/settings.py
+++ crawler/settings.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-BOT_NAME = 'crawler'
-SPIDER_MODULES = ['crawler.spiders']
-NEWSPIDER_MODULE = 'crawler.spiders'
-ROBOTSTXT_OBEY = False
-
 USER_AGENT = 'crawler'
-
-ITEM_PIPELINES = {
-   'crawler.pipelines.ItemCollectorPipeline': 300,
-}
 
 DB_USER = 'scraper'
 DB_PASS = 'scraper'
 DB_NAME = 'scraper'
 DB_HOST = '127.0.0.1'
 DB_PORT = '3306'
+
+BOT_NAME = 'crawler'
+SPIDER_MODULES = ['crawler.spiders']
+NEWSPIDER_MODULE = 'crawler.spiders'
+ROBOTSTXT_OBEY = False
+
+ITEM_PIPELINES = {
+	'crawler.pipelines.ItemCollectorPipeline': 300,
+}
