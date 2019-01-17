e4da99063fd6a8b01fd51ef787493ad889f45515
Radosław Przytuła <radoslaw.przytula@gmail.com>
2019-01-18 00:48:07 +0100

    prototype

--- crawler/spiders/generic.py
+++ crawler/spiders/generic.py
@@ -10,7 +10,7 @@ class GenericSpider(scrapy.Spider):
     name = 'generic'
 
     def __init__(self, connection, url_id):
-        result = connection.execute('SELECT url FROM urls WHERE id={}'.format(url_id,))
+        result = connection.execute('SELECT url FROM urls WHERE id={}'.format(int(url_id),))
         self.start_urls = [] + [r['url'] for r in result]
 
     def parse(self, response):
---
+++ uwsgi/handler.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+import subprocess
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from sqlalchemy.engine import create_engine
+
+from crawler.spiders.generic import GenericSpider
+
+config = get_project_settings()
+# hide scrapy logs
+logging.getLogger('scrapy').propagate = False
+
+def application(env, start_response):
+    uri = env['REQUEST_URI']
+    process = subprocess.Popen(['python3', 'start.py', uri.replace('/','')],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process.communicate()
+    start_response('200 OK', [('Content-Type','text/html')])
+    with open('response.txt') as r:
+        response = r.read()
+    return [response.encode('utf-8')]
